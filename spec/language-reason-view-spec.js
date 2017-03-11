e9218ff39212a653244f7899e7738d057c890d65
Cheng Lou <chenglou92@gmail.com>
2017-03-11 00:24:42 -0800

    Re-publish language-reason

--- spec/language-reason-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageReasonView from '../lib/language-reason-view';
-
-describe('LanguageReasonView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
